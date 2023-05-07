45586c020ef88526fa4e824e65de6b00f26d254a
Bharat Soni <bharatchills@gmail.com>
2023-05-07 14:37:41 +0530

    dragging-fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/gestures.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Dynamic Container'),
+          title: Text('Dynamic Container'),
         ),
-        body: const InteractiveContainer(),
+        body: InteractiveContainer(),
       ),
     );
   }
 }
 
 class InteractiveContainer extends StatefulWidget {
-  const InteractiveContainer({super.key});
-
   @override
-  InteractiveContainerState createState() => InteractiveContainerState();
+  _InteractiveContainerState createState() => _InteractiveContainerState();
 }
 
-class InteractiveContainerState extends State<InteractiveContainer> {
+class _InteractiveContainerState extends State<InteractiveContainer> {
   Offset position = Offset.zero;
   double containerWidth = 200;
   double containerHeight = 200;
   double scaleFactor = 0.01;
+  Offset? _scaleFocalPoint;
 
-  void _handleDragUpdate(DragUpdateDetails details) {
-    setState(() {
-      position += details.delta;
-    });
+  void _handleScaleStart(ScaleStartDetails details) {
+    _scaleFocalPoint = details.localFocalPoint;
   }
 
   void _handleScaleUpdate(ScaleUpdateDetails details) {
     setState(() {
+      // Handle scaling
       double newScale = 1 + (details.scale - 1) * scaleFactor;
       containerWidth *= newScale;
       containerHeight *= newScale;
+
+      // Handle dragging
+      position += details.localFocalPoint - _scaleFocalPoint!;
+      _scaleFocalPoint = details.localFocalPoint;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onPanUpdate: _handleDragUpdate,
-      child: RawGestureDetector(
-        gestures: {
-          ScaleGestureRecognizer:
-              GestureRecognizerFactoryWithHandlers<ScaleGestureRecognizer>(
-            () => ScaleGestureRecognizer(),
-            (ScaleGestureRecognizer instance) {
-              instance.onUpdate = _handleScaleUpdate;
-            },
-          ),
-        },
-        child: Stack(
-          children: [
-            Positioned(
-              left: position.dx,
-              top: position.dy,
-              child: Container(
-                width: containerWidth,
-                height: containerHeight,
-                color: Colors.blueAccent,
-              ),
+      onScaleStart: _handleScaleStart,
+      onScaleUpdate: _handleScaleUpdate,
+      child: Stack(
+        children: [
+          Positioned(
+            left: position.dx,
+            top: position.dy,
+            child: Container(
+              width: containerWidth,
+              height: containerHeight,
+              color: Colors.blueAccent,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
