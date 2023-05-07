d754838ed65a587bd55186a91a2709712ddf7e8f
Bharat Soni <bharatchills@gmail.com>
2023-05-07 15:03:08 +0530

    added button to resize

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Dynamic Container'),
+          title: const Text('Dynamic Container'),
         ),
-        body: InteractiveContainer(),
+        body: const InteractiveContainer(),
       ),
     );
   }
 }
 
 class InteractiveContainer extends StatefulWidget {
+  const InteractiveContainer({super.key});
+
   @override
-  _InteractiveContainerState createState() => _InteractiveContainerState();
+  InteractiveContainerState createState() => InteractiveContainerState();
 }
 
-class _InteractiveContainerState extends State<InteractiveContainer> {
+class InteractiveContainerState extends State<InteractiveContainer> {
   Offset position = Offset.zero;
   double containerWidth = 200;
   double containerHeight = 200;
@@ -61,10 +65,37 @@ class _InteractiveContainerState extends State<InteractiveContainer> {
               width: containerWidth,
               height: containerHeight,
               color: Colors.blueAccent,
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Transform.translate(
+                  offset: const Offset(12, 12),
+                  child: Container(
+                      decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black26,
+                                blurRadius: 4,
+                                offset: Offset(0, 2))
+                          ]),
+                      padding: const EdgeInsets.all(2),
+                      child: GestureDetector(
+                          onPanUpdate: _handleResizeUpdate,
+                          child: const Icon(Icons.drag_handle, size: 24))),
+                ),
+              ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  void _handleResizeUpdate(DragUpdateDetails details) {
+    setState(() {
+      containerWidth += details.delta.dx;
+      containerHeight += details.delta.dy;
+    });
   }
 }
